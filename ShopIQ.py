727a87be2d1e970433c6bfb123cf82cb6afa5e0b
Ketan Kumar <112306805+Ketan3102@users.noreply.github.com>
2023-08-16 22:57:28 +0530

    exception block

--- ShopIQ.py
+++ ShopIQ.py
@@ -38,36 +38,39 @@ def recommendation_emb(img,model):
     return normalized_embedding
 
 def main():
-    st.set_page_config(page_title='ShopIQ',page_icon='🛒')
-    st.title('ShopIQ: Your Shopping Buddy')
-    col_upload,col_dropdown=st.columns(2)
-    with col_upload:
-        data=st.file_uploader("Insert Your Image")
-    display_img=None
-    if data:
-        embeddings,image_paths=path_embeddings()
-        model=recomm_model()
-        display_img=Image.open(data).resize((224,224))
-        st.image(display_img)
-        recommendation_embedding=recommendation_emb(display_img,model)
-        neighbours=NearestNeighbors(n_neighbors=5,algorithm='brute',metric='euclidean')
-        neighbours.fit(embeddings)
-        dist,index=neighbours.kneighbors(recommendation_embedding)
-        st.subheader("You may also like:")
-        col1,col2,col3,col4,col5=st.columns(5)
-        with col1:
-            st.image(image_paths[index[0][0]])
-        with col2:
-            st.image(image_paths[index[0][1]])
-        with col3:
-            st.image(image_paths[index[0][2]])
-        with col4:
-            st.image(image_paths[index[0][3]])
-        with col5:
-            st.image(image_paths[index[0][4]])
+    try:
+        st.set_page_config(page_title='ShopIQ',page_icon='🛒')
+        st.title('ShopIQ: Your Shopping Buddy')
+        col_upload,col_dropdown=st.columns(2)
+        with col_upload:
+            data=st.file_uploader("Insert Image of Clothing Item")
+        display_img=None
+        if data:
+            embeddings,image_paths=path_embeddings()
+            model=recomm_model()
+            display_img=Image.open(data).resize((224,224))
+            st.image(display_img)
+            recommendation_embedding=recommendation_emb(display_img,model)
+            neighbours=NearestNeighbors(n_neighbors=5,algorithm='brute',metric='euclidean')
+            neighbours.fit(embeddings)
+            dist,index=neighbours.kneighbors(recommendation_embedding)
+            st.subheader("You may also like:")
+            col1,col2,col3,col4,col5=st.columns(5)
+            with col1:
+                st.image(image_paths[index[0][0]])
+            with col2:
+                st.image(image_paths[index[0][1]])
+            with col3:
+                st.image(image_paths[index[0][2]])
+            with col4:
+                st.image(image_paths[index[0][3]])
+            with col5:
+                st.image(image_paths[index[0][4]])
 
-    else:
-        st.warning('No image')
+        else:
+            st.warning('No image')
+    except Exception as e:
+        return st.error('No recommendation for this image. Please choose a valid image!!')
     
     
 
